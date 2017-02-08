48b060b5ce626020fd1bc17ff63fddd047d5d322
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-02-08 18:45:56 +0000

    add Github plugin with basic structure, tutorial part 1

---
+++ plugins/GithubBundle/Config/config.php
@@ -0,0 +1,9 @@
+<?php
+// plugins/GithubBundle/Config/config.php
+
+return array(
+    'name' => 'Github Integration',
+    'description' => 'An example plugin from Mautic tutorial, usins Github API',
+    'author' => 'Karen Freeman-Smith',
+    'version' => '1.0.0',
+    );
---
+++ plugins/GithubBundle/GithubBundle.php
@@ -0,0 +1,13 @@
+<?php
+// plugins/GithubBundle/GithubBundle.php
+
+namespace
+MauticPlugin\GithubBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class GithubBundle extends PluginBundleBase
+{
+    // code to do something here...
+}
+}
