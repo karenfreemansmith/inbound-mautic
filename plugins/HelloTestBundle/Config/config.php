76424970abe7051decffc8f6ece58f7b23310323
Karen Freeman Smith <karenfreemansmith@gmail.com>
2017-02-08 18:14:17 +0000

    first plugin

---
+++ plugins/HelloTestBundle/Config/config.php
@@ -0,0 +1,9 @@
+<?php
+// plugins/HelloTestBundle/Config/config.php
+
+return array(
+    'name' => 'Hello Test',
+    'description' => 'An example plugin with most basic structure',
+    'author' => 'Karen Freeman-Smith',
+    'version' => '1.0.0',
+    );
---
+++ plugins/HelloTestBundle/HelloTestBundle.php
@@ -0,0 +1,12 @@
+<?php
+// plugins/HelloTestBundle/HelloTestBundle.php
+
+namespace
+MauticPlugin\HelloTestBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class HelloTestBundle extends PluginBundleBase
+{
+    // code here to do something is optional for minimum funcationality
+}
